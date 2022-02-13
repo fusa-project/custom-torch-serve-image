d4cabd8f4aa9134e39c575dbbc4475fc811588e7
Pablo Huijse <pablo.huijse@gmail.com>
2022-02-13 18:50:24 -0300

    Add messages to future cron job

--- download_models.sh
+++ download_models.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+timestamp=`date +%Y/%m/%d-%H:%M:%S`
 gdown https://drive.google.com/drive/folders/1b9N-iNCC19lJm8hElqLetDqXTbPil2rd -O /root/custom-torch-serve-image/model-store --folder
+echo "Model folder synced, $timestamp"
