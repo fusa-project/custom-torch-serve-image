f4cc88d8ec278c784db26301b68e26b00b97c383
Pablo Huijse <pablo.huijse@gmail.com>
2022-02-13 19:07:46 -0300

    terraform friendly

--- cron_sync_gdrive.sh
+++
@@ -1,6 +0,0 @@
-SHELL=/bin/bash
-PATH=/usr/bin:/bin:/usr/local/bin
-MAILTO=phuijse@inf.uach.cl
-
-# Download models from gdrive every day at 0:00 AM
-0 0 * * * /root/custom-torch-serve-image/download_models.sh
